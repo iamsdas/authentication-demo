21b2fc73ee4b23f02b9fcc98f9d57f08044e4b8f
Suryashankar Das <suryashankardas.2002@gmail.com>
2021-07-07 20:03:21 +0530

    add signin/signup links

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -80,6 +80,10 @@ const Signin = () => {
             className='uppercase text-white border-4 border-gray-700 bg-gray-700 hover:border-gray-800 hover:bg-gray-800 rounded-xl px-3 py-2 font-semibold w-full'>
             sign in
           </button>
+          {/* sign up */}
+          <div className='pt-2'>
+            <a href='/signup'>Dont have an account? Sign up here</a>
+          </div>
         </form>
       </div>
     </div>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -115,6 +115,10 @@ const Signup = () => {
             className='uppercase text-white border-4 border-gray-700 bg-gray-700 hover:border-gray-800 hover:bg-gray-800 rounded-xl px-3 py-2 font-semibold w-full'>
             sign up
           </button>
+          {/* sign in */}
+          <div className='pt-2'>
+            <a href='/signin'>Already have an account? Sign in here</a>
+          </div>
         </form>
       </div>
     </div>

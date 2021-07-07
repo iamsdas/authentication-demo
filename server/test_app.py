78e81f578969a458aef7a2e51ed94172b744a1ff
Suryashankar Das <suryashankardas.2002@gmail.com>
2021-07-07 17:10:43 +0530

    add unit tests

---
+++ server/test_app.py
@@ -0,0 +1,39 @@
+from flask import json
+from flask.testing import FlaskClient
+import pytest
+from app import create_app
+
+
+@pytest.fixture
+def client():
+    app = create_app(testing=True)
+    with app.test_client() as client:
+        yield client
+
+
+# check response code of the signup API
+def test_1_signup_status(client: FlaskClient):
+    cases = [
+            [{'email': 'a@b.com',
+              'password': 'pass', 'first_name': 'a', 'last_name': 'b'}, 200],  # sucess
+            [{'email': 'a@b.com'}, 400],  # incomplete
+            [{'email': 'a@b.com',
+              'password': 'pass', 'first_name': 'a', 'last_name': 'b'}, 409]  # duplicate
+    ]
+    for (user_info, value) in cases:
+        response = client.post('/signup', json=user_info)
+        status = response.status_code
+        assert status == value
+
+
+# check response code of signin API
+def test_2_sigin_status(client: FlaskClient):
+    cases = [
+        [{'email': 'a@b.com', 'password': 'pass'}, 200],  # success
+        [{'email': 'a@b.com'}, 400],  # incomplete
+        [{'email': 'a@b.com', 'password': 'pa'}, 403]  # wrong
+    ]
+    for (user_info, value) in cases:
+        response = client.post('/signin', json=user_info)
+        status = response.status_code
+        assert status == value
